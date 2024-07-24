65a061682ec6e088afee0a689f84b9f2b5f09fab
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-07-24 12:06:45 +0530

    VideoTitle.js

--- Components/VideoTitle.js
+++ Components/VideoTitle.js
@@ -5,7 +5,7 @@
 
 function VideoTitle({ title, overview }) {
     return (
-        <div className=" text-gray-200 p-6 rounded-lg  pt-40 absolute">
+        <div className="w-screen aspect-video bg-gradient-to-r from-black text-gray-200 px-10 rounded-lg  pt-[14%] absolute">
             <h1 className="text-6xl font-bold mb-4">{title}</h1>
             <p className="text-xl mb-6 w-1/3">{overview}</p>
             <div className="flex space-x-4 bg-opacity-30">
