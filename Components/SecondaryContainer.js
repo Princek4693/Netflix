51567dc2f5f55f6a66cbc14c023f3c5107735217
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-07-24 12:05:43 +0530

    SecondaryContainer.js

--- Components/SecondaryContainer.js
+++ Components/SecondaryContainer.js
@@ -1,8 +1,21 @@
-function SecondaryContainer(){
-    return (
-        <div>
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
+
+function SecondaryContainer(){
+
+    const movies = useSelector((store) => store.movies)
+    return (
+        movies.nowPlayingMovie && 
+        <div className="bg-black">
+        <div className="-mt-56 relative z-20">
+        <MovieList title={"Now Playing Movies"} movies={movies.nowPlayingMovie}></MovieList>
+        <MovieList title={"Trending"} movies={movies.nowPlayingMovie}></MovieList>
+        <MovieList title={"Popular"} movies={movies.popularMovies}></MovieList>
+        <MovieList title={"Upcoming Movies"} movies={movies.upComingMovies}></MovieList>
+        <MovieList title={"Horror"} movies={movies.nowPlayingMovie}></MovieList>
         </div>
+    </div>
     )
 }
 
