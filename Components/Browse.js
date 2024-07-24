ad73bdc9580f4bce198ac48b940b52c49b028fbc
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-07-24 12:04:44 +0530

    Browse.js

--- Components/Browse.js
+++ Components/Browse.js
@@ -1,13 +1,34 @@
-import Header from "./Header";
 
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTrendingMovies from "../hooks/useTrendingMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import Header from "./Header";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 function Browse(){
+
+    //fetch the movies and updating the store
+    useNowPlayingMovies();
+    usePopularMovies();   
+    useTrendingMovies();
+    useUpcomingMovies();
     return (
         <div>
-            <Header></Header>
+            <Header />
+            <MainContainer></MainContainer>
+            <SecondaryContainer></SecondaryContainer>
+            {/* 
+            MainContainer
+                - VideoBackground
+                - VideoTitle
+            SecondaryContainer
+                - Movie list
+            
+            */}
         </div>
     )
 }
-
 
 export default Browse;
