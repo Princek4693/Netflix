c90838e473b85a9aff50e995d4c7d5bc38b616ba
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-07-24 12:06:17 +0530

    VideoBackground.js

--- Components/VideoBackground.js
+++ Components/VideoBackground.js
@@ -20,7 +20,8 @@ function VideoBackground({moviesId}){
         <div className="w-screen">
             <iframe 
             className="w-screen aspect-video"
-            src={"https://www.youtube.com/embed/" + trailerVideos?.key } title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+            src={"https://www.youtube.com/embed/" + trailerVideos?.key + "?&autoplay=1&mute=1" } 
+            title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin"></iframe>
             {/* it is a camel case be sure capital second word */}
         </div>
     )
