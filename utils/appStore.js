692a4292088d35d65cd87bcf85e3ee75a166b7c0
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-07-25 11:46:50 +0530

    appStore.js

--- utils/appStore.js
+++ utils/appStore.js
@@ -1,10 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import moviesSlice from "./moviesSlice";
+import GptSlice from "./GptSlice";
+import configReducer from './configSlice'
+
 
 const appStore = configureStore(
     {
         reducer : {
             user : userReducer,
+            movies : moviesSlice,
+            gpt : GptSlice,
+            config : configReducer
         }
     }
 )
