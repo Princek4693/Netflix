960a1f5f2036cea491f13592524bb46759901107
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-07-25 11:42:14 +0530

    Browse.js

--- Components/Browse.js
+++ Components/Browse.js
@@ -1,13 +1,19 @@
 
+import { useSelector } from "react-redux";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import usePopularMovies from "../hooks/usePopularMovies";
 import useTrendingMovies from "../hooks/useTrendingMovies";
 import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import GptSearch from "./GptSearch";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
 
 function Browse(){
+    
+
+    //use use selector to fetch from GptSlice
+    const GptSelector = useSelector((store) => store.gpt.GptSearchNow)
 
     //fetch the movies and updating the store
     useNowPlayingMovies();
@@ -17,16 +23,11 @@ function Browse(){
     return (
         <div>
             <Header />
-            <MainContainer></MainContainer>
-            <SecondaryContainer></SecondaryContainer>
-            {/* 
-            MainContainer
-                - VideoBackground
-                - VideoTitle
-            SecondaryContainer
-                - Movie list
-            
-            */}
+            {
+                GptSelector ?  <GptSearch></GptSearch> : 
+                <> <MainContainer></MainContainer>
+            <SecondaryContainer></SecondaryContainer></>
+            }
         </div>
     )
 }
